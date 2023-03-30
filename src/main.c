ffb0e229a2c3453479e948cb629566538bcbb36d
InshaadMerchant <114313560+InshaadMerchant@users.noreply.github.com>
2023-03-30 18:20:58 +0000

    Inshaad Merchant 1001861293

--- src/main.c
+++ src/main.c
@@ -43,12 +43,11 @@ uint8_t (*distance_calculated)[NUM_STARS];
 double min = FLT_MAX;
 double max = FLT_MIN;
 double distance = 0.0;
-uint64_t count_threads = 0;
 
-uint32_t thread_count = 1;
+uint32_t thread_count = 1; //Declaring and initializing a variable to count threads
 pthread_mutex_t mutex;
 
-int numOfThreads;
+int numOfThreads; //Variable to store number of threads used.
 int count;
 
 void showHelp()
@@ -69,45 +68,45 @@ float determineAverageAngularDistance(struct Star arr[], int start, int end)
     {
       if (i != j && distance_calculated[i][j] == 0)
       {
-        double spacing = calculateAngularDistance(arr[i].RightAscension, arr[i].Declination,
-                                               arr[j].RightAscension, arr[j].Declination);
+        double spacing = calculateAngularDistance(arr[i].RightAscension, arr[i].Declination, //Calculating spacing/distance
+                                               arr[j].RightAscension, arr[j].Declination);   //between stars
         distance_calculated[i][j] = 1;
         distance_calculated[j][i] = 1;
-        pthread_mutex_lock( &mutex );
+        pthread_mutex_lock( &mutex ); // Locking Mutex for a thread to execute
 
-        thread_count++;
+        thread_count++; //Incrementing thread counter
 
-        if (min > spacing)
+        if (min > spacing) //Determining Min Distance
         {
           min = spacing;
         }
 
-        if (max < spacing)
+        if (max < spacing) //Determining Max Distance
         {
           max = spacing;
         }
-        pthread_mutex_unlock( &mutex );
+        pthread_mutex_unlock( &mutex ); // Unlocking mutex after that thread has been executed.
       }
     }
   }
-  return distance;
+  return distance; //returning distance between stars
 }
 
 void *threading_Function(void *a)
 {
   int count = (intptr_t)a;
   int start = 0, end = 0;
-  int interval = NUM_STARS / numOfThreads;
+  int interval = NUM_STARS / numOfThreads; //Calculating interval between stars.
 
-  start = interval * count;
+  start = interval * count; 
   end = interval * (count + 1);
-  determineAverageAngularDistance(star_array, start, end);
+  determineAverageAngularDistance(star_array, start, end); // Calling Function to find avg angular distance.
   return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-  FILE *fp;
+  FILE *fp; //Declaring File Pointer
   uint32_t star_count = 0;
 
   uint32_t n;
@@ -135,7 +134,7 @@ int main(int argc, char *argv[])
       numOfThreads = atoi(argv[n + 1]);
     }
   }
-
+  //opening file and checking if it exists or not.
   fp = fopen("data/tycho-trimmed.csv", "r");
 
   if (fp == NULL)
@@ -177,20 +176,21 @@ int main(int argc, char *argv[])
   }
   printf("%d records read\n", star_count);
   pthread_t threads[numOfThreads];
-  clock_t NumOfClocks;
-  NumOfClocks = clock();
+  // Time Calculation starts here.............
+  clock_t NumOfClocks; // declaring a variable to count number of clocks
+  NumOfClocks = clock(); // clock() function included in time.h to count number of clocks
   // Find the average angular distance in the most inefficient way possible
-  clock_t start_time = clock();
-  pthread_mutex_init(&mutex, NULL);
+  clock_t start_time = clock(); // Record start time
+  pthread_mutex_init(&mutex, NULL); //Initializing Mutex
   for (int i = 0; i < numOfThreads; i++)
   {
-    pthread_create(&threads[i], NULL, threading_Function, (void *)(intptr_t)i);
+    pthread_create(&threads[i], NULL, threading_Function, (void *)(intptr_t)i); //Create a new thread.
   }
   for (int i = 0; i < numOfThreads; i++)
   {
-    pthread_join(threads[i], NULL);
+    pthread_join(threads[i], NULL); //Waiting for a thread to terminate.
   }
-  clock_t end_time = clock();
+  clock_t end_time = clock(); // Record end time
 
   double total = 0.0;
   uint64_t count = 0;
@@ -203,22 +203,22 @@ int main(int argc, char *argv[])
         distance = calculateAngularDistance(
             star_array[i].RightAscension, star_array[i].Declination,
             star_array[j].RightAscension, star_array[j].Declination);
-        total += distance;
-        count++;
+        total += distance; // Adding all the distances returned from calculateAngularDistance Function
+        count++; //Incrementing count for all the distances returned.
       }
     }
   }
 
-  double average_distance = total / count;
-
-  printf("Average distance found is %lf\n", average_distance);
+  double avg_distance = total / count; //Calculating average distance between stars
+  // Printing Results onto Screen...........
+  printf("Average distance found is %lf\n", avg_distance);
   printf("Minimum distance found is %lf\n", min);
   printf("Maximum distance found is %lf\n", max);
-  double time_Spent = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+  double time_Spent = ((double)(end_time - start_time)) / CLOCKS_PER_SEC; //Calculating time by (diff of start and end clocks)/CLOCKS_PER_SEC
   printf("Time Taken: %f seconds \n", time_Spent);
 
-  fclose(fp);
-  pthread_exit(NULL);
+  fclose(fp); //Closing File
+  pthread_exit(NULL); // Exiting pthread
 
   return 0;
 }
